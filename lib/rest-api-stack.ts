d1920913282859d3400e0067c795952948d6ea59
bkmashiro <53376445+bkmashiro@users.noreply.github.com>
2025-03-05 19:36:38 +0000

    Add API endpoint for posting a new movie.

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -54,6 +54,18 @@ export class RestAPIStack extends cdk.Stack {
       }
     );
 
+    const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_22_X,
+      entry: `${__dirname}/../lambdas/addMovie.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: moviesTable.tableName,
+        REGION: "eu-west-1",
+      },
+    });
+
     new custom.AwsCustomResource(this, "moviesddbInitData", {
       onCreate: {
         service: "DynamoDB",
@@ -73,6 +85,7 @@ export class RestAPIStack extends cdk.Stack {
     // Permissions
     moviesTable.grantReadData(getMovieByIdFn);
     moviesTable.grantReadData(getAllMoviesFn);
+    moviesTable.grantReadWriteData(newMovieFn);
 
     // REST API
     const api = new apig.RestApi(this, "RestAPI", {
@@ -94,11 +107,19 @@ export class RestAPIStack extends cdk.Stack {
       "GET",
       new apig.LambdaIntegration(getAllMoviesFn, { proxy: true })
     );
+
+    moviesEndpoint.addMethod(
+      "POST",
+      new apig.LambdaIntegration(newMovieFn, { proxy: true })
+    );
+    
     // Detail movie endpoint
     const specificMovieEndpoint = moviesEndpoint.addResource("{movieId}");
     specificMovieEndpoint.addMethod(
       "GET",
       new apig.LambdaIntegration(getMovieByIdFn, { proxy: true })
     );
+
+
   }
 }
